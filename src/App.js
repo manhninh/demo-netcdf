5822464fac3c50b0d117f89a57e55645adccb258
Ninh Pham <manhninh91@gmail.com>
2022-04-19 22:56:05 +0700

    123

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ import $ from "jquery";
 import "leaflet-velocity";
 import "./App.css";
 
+let map = null;
+
 function App() {
   useEffect(() => {
     if (map) return;
-    var map = L.map("map");
-    map.setView([21.04549, 105.76257], 6);
 
-    L.tileLayer(
+    const baseLayer = L.tileLayer(
       "https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFuaG5pbmg5MSIsImEiOiJjazl3ODMyZjMwNzU0M2txNmJnMXh1cDk2In0.XHVqDEN-v2YVftKn5IAwsg",
       {
         attribution:
@@ -22,11 +22,20 @@ function App() {
         id: "mapbox/dark-v10",
         tileSize: 512,
         zoomOffset: -1,
-        accessToken: "pk.eyJ1IjoibWFuaG5pbmg5MSIsImEiOiJjazl3ODMyZjMwNzU0M2txNmJnMXh1cDk2In0.XHVqDEN-v2YVftKn5IAwsg",
+        accessToken:
+          "pk.eyJ1IjoibWFuaG5pbmg5MSIsImEiOiJjazl3ODMyZjMwNzU0M2txNmJnMXh1cDk2In0.XHVqDEN-v2YVftKn5IAwsg",
       }
-    ).addTo(map);
+    );
+    map = L.map("map", { layers: baseLayer });
+    map.setView([21.04549, 105.76257], 6);
 
-    L.tileLayer
+    var baseLayers = {
+      "Base map": baseLayer,
+    };
+    var layerControl = L.control.layers(baseLayers);
+    layerControl.addTo(map);
+
+    const tLayer = L.tileLayer
       .wms("http://103.27.239.181:8080/geoserver/SMAP/wms", {
         format: "image/png",
         VERSION: "1.1.1",
@@ -38,6 +47,8 @@ function App() {
         transparent: true,
       })
       .addTo(map);
+
+    layerControl.addOverlay(tLayer, "Nhiệt độ");
 
     $.getJSON(`${process.env.PUBLIC_URL}/wind0.json`, function (data) {
       var velocityLayer = L.velocityLayer({
@@ -51,9 +62,36 @@ function App() {
         data: data,
         maxVelocity: 10,
       });
-      map.addLayer(velocityLayer);
+      layerControl.addOverlay(velocityLayer, "Gió");
     });
+
+    map.addEventListener("click", onMapClick);
   }, []);
+
+  const onMapClick = (e) => {
+    const popup = new L.Popup({ maxWidth: 400 });
+    var BBOX =
+      map.getBounds()._southWest.lng +
+      "," +
+      map.getBounds()._southWest.lat +
+      "," +
+      map.getBounds()._northEast.lng +
+      "," +
+      map.getBounds()._northEast.lat;
+    var WIDTH = map.getSize().x;
+    var HEIGHT = map.getSize().y;
+    var X = map.layerPointToContainerPoint(e.layerPoint).x;
+    var Y = map.layerPointToContainerPoint(e.layerPoint).y;
+    var URL =
+      `http://103.27.239.181:8080/geoserver/SMAP/wms?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetFeatureInfo&FORMAT=image/jpeg&TRANSPARENT=true&QUERY_LAYERS=SMAP:t&STYLES&LAYERS=SMAP:t&exceptions=application/vnd.ogc.se_inimage&INFO_FORMAT=text/html&FEATURE_COUNT=50&X=${X}&Y=${Y}&SRS=EPSG:4326&WIDTH=${WIDTH}&HEIGHT=${HEIGHT}&BBOX=${BBOX}`;
+    popup.setLatLng(e.latlng);
+    popup.setContent(
+      "<iframe src='" +
+        URL +
+        "' width='400' height='100' frameborder='0'></iframe>"
+    );
+    map.openPopup(popup);
+  };
 
   return (
     <div
