86644840efe432dd290c066a5272aa47ae25b075
ninh.pham <ninh.pham.manh@247post.vn>
2022-04-29 14:07:45 +0700

    udate code

--- src/App.js
+++ src/App.js
@@ -26,8 +26,7 @@ function App() {
         id: "mapbox/dark-v10",
         tileSize: 512,
         zoomOffset: -1,
-        accessToken:
-          "pk.eyJ1IjoibWFuaG5pbmg5MSIsImEiOiJjazl3ODMyZjMwNzU0M2txNmJnMXh1cDk2In0.XHVqDEN-v2YVftKn5IAwsg",
+        accessToken: "pk.eyJ1IjoibWFuaG5pbmg5MSIsImEiOiJjazl3ODMyZjMwNzU0M2txNmJnMXh1cDk2In0.XHVqDEN-v2YVftKn5IAwsg",
       }
     ).addTo(map);
     map.addEventListener("click", onMapClick);
@@ -38,10 +37,7 @@ function App() {
     let tocdoGio = "";
     let nhietdo = "";
     if (layerWind) {
-      const gridValue = layerWind.field.interpolatedValueAt(
-        e.latlng.lng,
-        e.latlng.lat
-      );
+      const gridValue = layerWind.field.interpolatedValueAt(e.latlng.lng, e.latlng.lat);
       if (gridValue && !isNaN(gridValue.u) && !isNaN(gridValue.v)) {
         const direction = getDirection(gridValue.u, gridValue.v, "bearingCCW");
         huongGio = "Hướng gió: " + direction.toFixed(2) + "°";
@@ -50,59 +46,58 @@ function App() {
       }
     }
     if (layerTems) {
+      console.log(layerTems, "layerTems");
       $.ajax({
-        url: `http://103.27.239.181:8080/wms`,
+        url: `http://103.237.147.62:8080/geoserver/smap_netcdf/wms`,
         method: "GET",
         async: false,
-        dataType: "html",
+        dataType: "json",
         data: {
-          layers: "demo/t2m",
-          QUERY_LAYERS: "demo/t2m",
-          styles: "default-scalar/seq-Heat-inv",
-          INFO_FORMAT: "text/html",
+          layers: layerTems.wmsParams.layers,
+          QUERY_LAYERS: layerTems.wmsParams.layers,
+          styles: "",
+          INFO_FORMAT: "application/json",
           FEATURE_COUNT: 5,
           REQUEST: "GetFeatureInfo",
           VERSION: "1.1.1",
           SRS: "EPSG:4326",
           BBOX: "97.617044,4.054536,126.355681,27.045445",
-          HEIGHT: 600,
-          WIDTH: 750,
+          HEIGHT: map.getSize().y,
+          WIDTH: map.getSize().x,
           X: e.containerPoint.x,
           Y: e.containerPoint.y,
         },
         success: function (xml) {
-          nhietdo = $(xml).find("tbody td:last");
+          nhietdo = Number(xml.features[0].properties.t2m).toFixed(0);
         },
       });
     }
-    if (huongGio || tocdoGio) {
+    if (huongGio || tocdoGio||nhietdo) {
       console.log(nhietdo, "nhietdo");
       const popup = new L.Popup({ maxWidth: 500 });
       popup.setLatLng(e.latlng);
       popup.setContent(`
     <p>${huongGio}</p>
     <p>${tocdoGio}</p>
-    ${nhietdo ? `<p>Nhiệt độ: ${nhietdo?.html()} °K</p>` : ""}
+    <p>Nhiệt độ: ${nhietdo} °K</p>
     `);
       map.openPopup(popup);
     }
   };
 
-  const loadData = (time) => () => {
+  const loadData = (time, level) => () => {
     if (layerWind) map.removeLayer(layerWind);
     if (layerTems) map.removeLayer(layerTems);
     layerWind = null;
     layerTems = null;
-    layerTems = L.tileLayer.wms(`http://103.27.239.181:8080/wms`, {
-      layers: "demo/t2m",
-      styles: "default-scalar/seq-Heat-inv",
+    layerTems = L.tileLayer.wms(`http://103.237.147.62:8080/geoserver/smap_netcdf/wms`, {
+      format: 'image/png',
+      version: "1.1.1",
+      tiled: true,
+      styles: "",
+      layers: `smap_netcdf:time${level}`,
+      exceptions: "application/vnd.ogc.se_inimage",
       transparent: true,
-      format: "image/png",
-      crs: L.CRS.EPSG4326,
-      TIME: moment(time, "DDMMYYYYHHmmss").format("YYYY-MM-DDTHH:mm:ss"),
-      COLORSCALERANGE: "263.4,306.7",
-      NUMCOLORBANDS: 250,
-      BGCOLOR: "transparent",
     });
 
     map.addLayer(layerTems);
@@ -165,134 +160,56 @@ function App() {
         }}
       >
         <div>
-          <input
-            type="radio"
-            name="wind"
-            onClick={loadData("25012022120000")}
-          />{" "}
-          {moment("25012022120000", "DDMMYYYYHHmmss").format(
-            "DD/MM/YYYY HH:mm:ss"
-          )}
+          <input type="radio" name="wind" onClick={loadData("25012022120000", 0)} />{" "}
+          {moment("25012022120000", "DDMMYYYYHHmmss").format("DD/MM/YYYY HH:mm:ss")}
         </div>
         <div>
-          <input
-            type="radio"
-            name="wind"
-            onClick={loadData("25012022180000")}
-          />{" "}
-          {moment("25012022180000", "DDMMYYYYHHmmss").format(
-            "DD/MM/YYYY HH:mm:ss"
-          )}
+          <input type="radio" name="wind" onClick={loadData("25012022180000", 1)} />{" "}
+          {moment("25012022180000", "DDMMYYYYHHmmss").format("DD/MM/YYYY HH:mm:ss")}
         </div>
         <div>
-          <input
-            type="radio"
-            name="wind"
-            onClick={loadData("26012022000000")}
-          />{" "}
-          {moment("26012022000000", "DDMMYYYYHHmmss").format(
-            "DD/MM/YYYY HH:mm:ss"
-          )}
+          <input type="radio" name="wind" onClick={loadData("26012022000000", 2)} />{" "}
+          {moment("26012022000000", "DDMMYYYYHHmmss").format("DD/MM/YYYY HH:mm:ss")}
         </div>
         <div>
-          <input
-            type="radio"
-            name="wind"
-            onClick={loadData("26012022060000")}
-          />{" "}
-          {moment("26012022060000", "DDMMYYYYHHmmss").format(
-            "DD/MM/YYYY HH:mm:ss"
-          )}
+          <input type="radio" name="wind" onClick={loadData("26012022060000", 3)} />{" "}
+          {moment("26012022060000", "DDMMYYYYHHmmss").format("DD/MM/YYYY HH:mm:ss")}
         </div>
         <div>
-          <input
-            type="radio"
-            name="wind"
-            onClick={loadData("26012022120000")}
-          />{" "}
-          {moment("26012022120000", "DDMMYYYYHHmmss").format(
-            "DD/MM/YYYY HH:mm:ss"
-          )}
+          <input type="radio" name="wind" onClick={loadData("26012022120000", 4)} />{" "}
+          {moment("26012022120000", "DDMMYYYYHHmmss").format("DD/MM/YYYY HH:mm:ss")}
         </div>
         <div>
-          <input
-            type="radio"
-            name="wind"
-            onClick={loadData("26012022180000")}
-          />{" "}
-          {moment("26012022180000", "DDMMYYYYHHmmss").format(
-            "DD/MM/YYYY HH:mm:ss"
-          )}
+          <input type="radio" name="wind" onClick={loadData("26012022180000", 5)} />{" "}
+          {moment("26012022180000", "DDMMYYYYHHmmss").format("DD/MM/YYYY HH:mm:ss")}
         </div>
         <div>
-          <input
-            type="radio"
-            name="wind"
-            onClick={loadData("27012022000000")}
-          />{" "}
-          {moment("27012022000000", "DDMMYYYYHHmmss").format(
-            "DD/MM/YYYY HH:mm:ss"
-          )}
+          <input type="radio" name="wind" onClick={loadData("27012022000000", 6)} />{" "}
+          {moment("27012022000000", "DDMMYYYYHHmmss").format("DD/MM/YYYY HH:mm:ss")}
         </div>
         <div>
-          <input
-            type="radio"
-            name="wind"
-            onClick={loadData("27012022060000")}
-          />{" "}
-          {moment("27012022060000", "DDMMYYYYHHmmss").format(
-            "DD/MM/YYYY HH:mm:ss"
-          )}
+          <input type="radio" name="wind" onClick={loadData("27012022060000", 7)} />{" "}
+          {moment("27012022060000", "DDMMYYYYHHmmss").format("DD/MM/YYYY HH:mm:ss")}
         </div>
         <div>
-          <input
-            type="radio"
-            name="wind"
-            onClick={loadData("27012022120000")}
-          />{" "}
-          {moment("27012022120000", "DDMMYYYYHHmmss").format(
-            "DD/MM/YYYY HH:mm:ss"
-          )}
+          <input type="radio" name="wind" onClick={loadData("27012022120000", 8)} />{" "}
+          {moment("27012022120000", "DDMMYYYYHHmmss").format("DD/MM/YYYY HH:mm:ss")}
         </div>
         <div>
-          <input
-            type="radio"
-            name="wind"
-            onClick={loadData("27012022180000")}
-          />{" "}
-          {moment("27012022180000", "DDMMYYYYHHmmss").format(
-            "DD/MM/YYYY HH:mm:ss"
-          )}
+          <input type="radio" name="wind" onClick={loadData("27012022180000", 9)} />{" "}
+          {moment("27012022180000", "DDMMYYYYHHmmss").format("DD/MM/YYYY HH:mm:ss")}
         </div>
         <div>
-          <input
-            type="radio"
-            name="wind"
-            onClick={loadData("28012022000000")}
-          />{" "}
-          {moment("28012022000000", "DDMMYYYYHHmmss").format(
-            "DD/MM/YYYY HH:mm:ss"
-          )}
+          <input type="radio" name="wind" onClick={loadData("28012022000000", 10)} />{" "}
+          {moment("28012022000000", "DDMMYYYYHHmmss").format("DD/MM/YYYY HH:mm:ss")}
         </div>
         <div>
-          <input
-            type="radio"
-            name="wind"
-            onClick={loadData("28012022060000")}
-          />{" "}
-          {moment("28012022060000", "DDMMYYYYHHmmss").format(
-            "DD/MM/YYYY HH:mm:ss"
-          )}
+          <input type="radio" name="wind" onClick={loadData("28012022060000", 11)} />{" "}
+          {moment("28012022060000", "DDMMYYYYHHmmss").format("DD/MM/YYYY HH:mm:ss")}
         </div>
         <div>
-          <input
-            type="radio"
-            name="wind"
-            onClick={loadData("28012022120000")}
-          />{" "}
-          {moment("28012022120000", "DDMMYYYYHHmmss").format(
-            "DD/MM/YYYY HH:mm:ss"
-          )}
+          <input type="radio" name="wind" onClick={loadData("28012022120000", 12)} />{" "}
+          {moment("28012022120000", "DDMMYYYYHHmmss").format("DD/MM/YYYY HH:mm:ss")}
         </div>
       </div>
     </>
